63cc83b2715c3b89bc7349e814ed1297592ac02d
nosirov123 <nosirovbilol92@gmail.com>
2025-05-05 12:01:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+// // Shu o'zgruvchilarni type larini console ga chiqaring
+// let mars = "Hello";
+// console.log(typeof mars);
+
+// let bu = 123;
+// console.log(typeof bu);
+
+// let kelajak = true;
+// console.log(typeof kelajak);
+
+// let mars1 = 123n;
+// console.log(typeof mars1);
+
+// let marsItSchool;
+// console.log(typeof marsItSchool);
+
+// let frontend = null;
+// console.log(typeof frontend);
+
+
+// let salomDunyo = Symbol("id");
+// console.log(typeof salomDunyo);
+
+// let obj = { name: "Ali" };
+// console.log(typeof obj);
+
+
+let ism = prompt('String typedagi malumot kiriting')
+console.log(typeof ism);
+
+let son = Number (prompt('Number typedagi malumot kiriting'))
+console.log(typeof son);
+
+let obj = Boolean (prompt('boolean typedagi malumot kiriting'))
+console.log(typeof obj);
+
+confirm  ('hamma malumotlarni togri kiritganingizga ishonchingiz komilmi?')
+
+alert  (' Siz kiritgan ' + ism + ' sozi String typega tegishli \n' + ' Siz kiritgan ' + son + ' sozi Number typega tegishli \n' + ' Siz kiritgan ' + obj + ' sozi Boolean typega tegishli')
